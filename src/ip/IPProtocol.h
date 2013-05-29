14f728efaa00a898670d55a11745e018ea32795a
Luis Campo Giralte <luis.camp0.2009@gmail.com>
2013-05-29 21:18:31 +0200

    modify the unit tests

--- src/ip/IPProtocol.h
+++ src/ip/IPProtocol.h
@@ -47,8 +47,8 @@ public:
 	static const int header_size = 20;
 
     	inline u_int8_t getTTL() const { return ip_header_->ttl; }
-    	inline u_int32_t getIPpktLength() const { return ntohs(ip_header_->tot_len); }
-    	inline u_int16_t getIPhdrLength() const { return ip_header_->ihl * 4; }
+    	inline u_int32_t getIPPacketLength() const { return ntohs(ip_header_->tot_len); }
+    	inline u_int16_t getIPHeaderLength() const { return ip_header_->ihl * 4; }
     	inline bool isIP() const { return ip_header_ ? true : false ; }
     	inline bool isIPver4() const { return ip_header_->version == 4; }
     	inline bool isFragment() const { return (ntohs(ip_header_->frag_off) & 0x3fff); }
@@ -59,8 +59,8 @@ public:
     	inline u_int32_t getDstAddr() const { return ip_header_->daddr; }
     	inline const char* getSrcAddrDotNotation() const { in_addr a; a.s_addr=ip_header_->saddr; return inet_ntoa(a); }
     	inline const char* getDstAddrDotNotation() const { in_addr a; a.s_addr=ip_header_->daddr; return inet_ntoa(a); }
-    	inline u_int32_t getIPpayloadLength() const { return getIPpktLength() - getIPhdrLength(); }
-    	//inline const char* getIPpayload() const { return (char*)payload + getIPhdrLength(); }
+    	inline u_int32_t getIPPayloadLength() const { return getIPPacketLength() - getIPHeaderLength(); }
+    	//inline const char* getIPPayload() const { return (char*)payload + getIPHeaderLength(); }
 private:
 	struct iphdr *ip_header_;
 };
--- src/ip/test_ip.cc
+++ src/ip/test_ip.cc
@@ -51,14 +51,18 @@ BOOST_AUTO_TEST_CASE (test2_ip) // ethernet -> ip
         mux_eth->addUpMultiplexer(mux_ip,ETHERTYPE_IP);
         mux_ip->addDownMultiplexer(mux_eth);	
 
-        // Sets the raw packet to a valid ethernet header
-        eth->setEthernetHeader(packet);
+	mux_eth->setPacketInfo(0,packet,length);
+	eth->setEthernetHeader(mux_eth->getRawPacket());     
+	// Sets the raw packet to a valid ethernet header
         BOOST_CHECK(eth->getEthernetType() == ETH_P_IP);
 
 	// executing the packet
 	// forward the packet through the multiplexers
         mux_eth->setPacketInfo(0,packet,length);
         mux_eth->forward();	
+
+        BOOST_CHECK(mux_eth->getPacketLength() == length);
+        BOOST_CHECK(mux_ip->getPacketLength() == length - 14);
 
 	delete ip;
 	delete eth;
@@ -107,27 +111,95 @@ BOOST_AUTO_TEST_CASE (test3_ip) // ethernet -> vlan -> ip
 	// executing the packet
 	// forward the packet through the multiplexers
         mux_eth->setPacketInfo(0,packet,length);
-        mux_eth->forward();	
+    	eth->setEthernetHeader(mux_eth->getRawPacket()); 
+	mux_eth->forward();	
 
-	BOOST_CHECK(vlan->getTotalPackets() == 1);
+	BOOST_CHECK(vlan->getTotalPackets() == 0);
 	BOOST_CHECK(ip->getTotalPackets() == 1);
 
         BOOST_CHECK(mux_eth->getTotalForwardPackets() == 1);
-        BOOST_CHECK(mux_vlan->getTotalForwardPackets() == 1);
-        BOOST_CHECK(mux_ip->getTotalForwardPackets() == 0);
-        BOOST_CHECK(mux_ip->getTotalFailPackets() == 1);
+        BOOST_CHECK(mux_vlan->getTotalForwardPackets() == 0);
+        BOOST_CHECK(mux_ip->getTotalForwardPackets() == 1);
+        BOOST_CHECK(mux_vlan->getTotalFailPackets() == 1);
 
+	std::cout << ip->getIPPacketLength() << std::endl;
         BOOST_CHECK(mux_eth->getPacketLength() == length);
         BOOST_CHECK(mux_vlan->getPacketLength() == length - 14);
-        BOOST_CHECK(mux_ip->getPacketLength() == length - 14 - 20);
+        BOOST_CHECK(mux_ip->getPacketLength() == length - (14 + 4 ));
 
-        //vl->setVLanHeader(mux_vlan->getRawPacket());
+        BOOST_CHECK(ip->getIPPacketLength() == mux_ip->getPacketLength());
         BOOST_CHECK(eth->getEthernetType() == ETH_P_8021Q);
         BOOST_CHECK(vlan->getEthernetType() == ETH_P_IP);
 
 	delete ip;
 	delete vlan;
 	delete eth;
+}
+
+// This test is similar to test2_ip but with another multiplexer plugin
+BOOST_AUTO_TEST_CASE (test4_ip) // ethernet -> vlan -> ip
+{
+        EthernetProtocol *eth = new EthernetProtocol();
+        MultiplexerPtr mux_eth = MultiplexerPtr(new Multiplexer());
+        VLanProtocol *vlan = new VLanProtocol();
+        MultiplexerPtr mux_vlan = MultiplexerPtr(new Multiplexer());
+        IPProtocol *ip = new IPProtocol();
+        MultiplexerPtr mux_ip = MultiplexerPtr(new Multiplexer());
+
+        // ethernet -> vlan ->ip ->udp -> dns
+        char *raw_packet = "\x00\x0c\x29\x2e\x3c\x2a\x90\x84\x0d\x62\xd8\x04" "\x08\x00"
+                "\x45\x00"
+                "\x00\x3d\x8a\x0d\x00\x00\xec\x11\xf4\x4f\xc0\xa8\x01\x76\x50\x3a"
+                "\x3d\xfa\xe9\xb3\x00\x35\x00\x29\x05\x94\x84\xd3\x01\x00\x00\x01"
+                "\x00\x00\x00\x00\x00\x00\x02\x63\x68\x04\x70\x6f\x6f\x6c\x03\x6e"
+                "\x74\x70\x03\x6f\x72\x67\x00\x00\x01\x00\x01";
+        int length = 75;
+        unsigned char *packet = reinterpret_cast <unsigned char*> (raw_packet);
+
+        //configure the eth
+        eth->setMultiplexer(mux_eth);
+        mux_eth->setHeaderSize(eth->header_size);
+        mux_eth->addChecker(std::bind(&EthernetProtocol::ethernetChecker,eth));
+
+        //configure the vlan
+        vlan->setMultiplexer(mux_vlan);
+        mux_vlan->setHeaderSize(vlan->header_size);
+        mux_vlan->addChecker(std::bind(&VLanProtocol::vlanChecker,vlan));
+
+        // configure the ip
+        ip->setMultiplexer(mux_ip);
+        mux_ip->setHeaderSize(ip->header_size);
+        mux_ip->addChecker(std::bind(&IPProtocol::ipChecker,ip));
+
+        // configure the multiplexers
+        mux_eth->addUpMultiplexer(mux_vlan,ETH_P_8021Q);
+        mux_vlan->addDownMultiplexer(mux_eth);
+        mux_eth->addUpMultiplexer(mux_ip,ETHERTYPE_IP);
+        mux_ip->addDownMultiplexer(mux_eth);
+
+        // executing the packet
+        // forward the packet through the multiplexers
+        mux_eth->setPacketInfo(0,packet,length);
+        eth->setEthernetHeader(mux_eth->getRawPacket());
+        mux_eth->forward();
+        
+	BOOST_CHECK(vlan->getTotalPackets() == 0);
+        BOOST_CHECK(ip->getTotalPackets() == 1);
+
+        BOOST_CHECK(mux_eth->getTotalForwardPackets() == 1);
+        BOOST_CHECK(mux_vlan->getTotalForwardPackets() == 1);
+        BOOST_CHECK(mux_ip->getTotalForwardPackets() == 0);
+        BOOST_CHECK(mux_ip->getTotalFailPackets() == 1);
+
+        std::cout << ip->getIPPacketLength() << std::endl;
+        BOOST_CHECK(mux_eth->getPacketLength() == length);
+        BOOST_CHECK(mux_vlan->getPacketLength() == length - 14);
+        BOOST_CHECK(mux_ip->getPacketLength() == length - (14 + 4 ));
+
+        BOOST_CHECK(ip->getIPPacketLength() == mux_ip->getPacketLength());
+        BOOST_CHECK(eth->getEthernetType() == ETH_P_8021Q);
+        BOOST_CHECK(vlan->getEthernetType() == ETH_P_IP);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
