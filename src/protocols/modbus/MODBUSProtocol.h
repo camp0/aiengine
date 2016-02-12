28752f2c2c8b80f9980ec7d3859685a7218eff1a
Luis <luis@localhost.localdomain>
2016-02-12 22:34:44 +0000

    Add modbus support first commits

---
+++ src/protocols/modbus/MODBUSProtocol.h
@@ -0,0 +1,133 @@
+/*
+ * AIEngine a deep packet inspector reverse engineering engine.
+ *
+ * Copyright (C) 2013-2016  Luis Campo Giralte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ * Written by Luis Campo Giralte <luis.camp0.2009@gmail.com> 
+ *
+ */
+#ifndef SRC_PROTOCOLS_MODBUS_MODBUSPROTOCOL_H_
+#define SRC_PROTOCOLS_MODBUS_MODBUSPROTOCOL_H_
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "Protocol.h"
+#include <arpa/inet.h>
+
+namespace aiengine {
+
+struct modbus_tcphdr {
+	uint16_t 	op;		/* Transaction id */
+	uint16_t 	proto;		/* Protocol id */
+	uint16_t 	length;		/* Transaction id */
+	uint8_t 	unitid;		/* Unit id */
+} __attribute__((packed));
+
+struct modbus_hdr {
+        uint8_t       	code;           /* Function code */
+        uint16_t       	proto;          /* Ref number */
+	u_char 		data[0];
+} __attribute__((packed));
+
+enum modbus_type_function_code {
+	MODBUSDISCOVER = 1,
+	MODBUSOFFER,
+	MODBUSREQUEST,
+	MODBUSDECLINE,
+	MODBUSACK,
+	MODBUSNAK,
+	MODBUSRELEASE,
+	MODBUSINFORM
+};
+
+class MODBUSProtocol: public Protocol 
+{
+public:
+    	explicit MODBUSProtocol():
+		Protocol("MODBUSProtocol","modbus"),
+		stats_level_(0),
+		modbus_header_(nullptr),total_bytes_(0)
+        	{}
+
+    	virtual ~MODBUSProtocol() {}
+
+	static const uint16_t id = 0;	
+	static constexpr int header_size = sizeof(struct modbus_tcphdr);
+
+	int getHeaderSize() const { return header_size;}
+
+	int64_t getTotalBytes() const { return total_bytes_;}
+	int64_t getTotalPackets() const { return total_packets_;}
+	int64_t getTotalValidatedPackets() const { return total_validated_packets_;}
+	int64_t getTotalMalformedPackets() const { return total_malformed_packets_;}
+
+        void processFlow(Flow *flow);
+        bool processPacket(Packet& packet) { return true; } 
+
+	void setStatisticsLevel(int level) { stats_level_ = level;}
+	void statistics(std::basic_ostream<char>& out);
+	void statistics() { statistics(std::cout);}
+
+	void releaseCache() {} // No need to free cache
+
+	void setHeader(unsigned char *raw_packet){ 
+
+		modbus_header_ = reinterpret_cast <struct modbus_tcphdr*> (raw_packet);
+	}
+
+	// Condition for say that a packet is dhcp 
+	bool modbusChecker(Packet &packet){ 
+	
+		int length = packet.getLength();
+
+		if(length >= header_size) {
+			if ((packet.getSourcePort() == 502)||(packet.getDestinationPort() == 502)) {
+				setHeader(packet.getPayload());
+				++total_validated_packets_; 
+				return true;
+			}
+		}
+		++total_malformed_packets_;
+		return false;
+	}
+
+	int64_t getAllocatedMemory() const { return sizeof(MODBUSProtocol); }
+	
+#if defined(PYTHON_BINDING)
+        boost::python::dict getCounters() const;
+#elif defined(RUBY_BINDING)
+	VALUE getCounters() const;
+#elif defined(JAVA_BINDING)
+        JavaCounters getCounters() const  { JavaCounters counters; return counters; }
+#endif
+
+private:
+	int stats_level_;
+	struct modbus_tcphdr *modbus_header_;
+	int64_t total_bytes_;
+        
+	// Some statistics 
+};
+
+typedef std::shared_ptr<MODBUSProtocol> MODBUSProtocolPtr;
+
+} // namespace aiengine
+
+#endif  // SRC_PROTOCOLS_MODBUS_MODBUSPROTOCOL_H_
---
+++ src/protocols/modbus/test_modbus.cc
@@ -0,0 +1,58 @@
+/*
+ * AIEngine a deep packet inspector reverse engineering engine.
+ *
+ * Copyright (C) 2013-2016  Luis Campo Giralte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ *
+ * Written by Luis Campo Giralte <luis.camp0.2009@gmail.com> 
+ *
+ */
+#include "test_modbus.h"
+
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#define BOOST_TEST_MODULE modbustest
+#endif
+#include <boost/test/unit_test.hpp>
+
+using namespace aiengine;
+
+BOOST_FIXTURE_TEST_SUITE(modbus_suite,StackMODBUStest)
+
+BOOST_AUTO_TEST_CASE (test1_modbus)
+{
+        unsigned char *pkt = reinterpret_cast <unsigned char*> (raw_packet_ethernet_ip_udp_dhcp_request);
+        int length = raw_packet_ethernet_ip_udp_dhcp_request_length;
+        Packet packet(pkt,length);
+
+        // executing the packet
+        // forward the packet through the multiplexers
+        mux_eth->setPacket(&packet);
+        eth->setHeader(mux_eth->getCurrentPacket()->getPayload());
+        mux_eth->setNextProtocolIdentifier(eth->getEthernetType());
+        mux_eth->forwardPacket(packet);
+
+        // Check the results
+        BOOST_CHECK(ip->getTotalPackets() == 1);
+        BOOST_CHECK(ip->getTotalValidatedPackets() == 1);
+        BOOST_CHECK(ip->getTotalBytes() == 300 + 20 + 8);
+        BOOST_CHECK(ip->getTotalMalformedPackets() == 0);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
